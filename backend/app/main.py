860dfe713daf71228cafeb577e7c0a0d370553b2
Deep-Saha1925 <dip23447@gmail.com>
2026-08-20 13:16:29 +0530

    Updated main.py file

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
 
+from app.database import Base, engine
+from app.models import Account, Transaction
+
+from app.routes.accounts import router as account_router
+from app.routes.transactions import router as transaction_router
+
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI(
     title="Fraud Protection System",
     description="Evidence-based fraud transaction analysis",
     version="1.0.0"
 )
+
+app.include_router(account_router)
+app.include_router(transaction_router)
 
 
 @app.get("/")
