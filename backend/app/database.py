3a0f7569365408bd998a291514e027ff2ac24995
Deep-Saha1925 <dip23447@gmail.com>
2026-08-21 14:40:35 +0530

    Updated code.

--- backend/app/database.py
+++ backend/app/database.py
@@ -7,7 +7,6 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-
 if not DATABASE_URL:
     raise RuntimeError("DATABASE_URL is not set")
 
